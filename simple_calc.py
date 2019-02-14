7d2c988aa2f7342d24c7cb6446155d529de31713
prem023 <47606671+prem0023@users.noreply.github.com>
2019-02-14 22:30:03 +0530

    simple calculator

---
+++ simple_calc.py
@@ -0,0 +1,37 @@
+def add(x, y):
+    return x+y
+
+def subtract(x, y):
+    return x-y
+
+def multiply(x, y):
+    return x*y
+
+def divide(x, y):
+    return x/y
+
+print("select operation")
+print("1.addition")
+print("2.subtraction")
+print("3.multiplication")
+print("4.division")
+
+choice = input("Enter choice (1/2/3/4):")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if choice == '1':
+    print(num1, "+", num2, "=", add(num1, num2))
+
+elif choice == '2':
+    print(num1, "-", num2, "=", subtract(num1, num2))
+
+elif choice == '3':
+    print(num1, "*", multiply(num1, num2))
+
+elif choice == '4':
+    print(num1, "/", num2, "=", divide(num1, num2))
+
+else:
+    print("invalid operation")

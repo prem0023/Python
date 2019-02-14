baeaf4186acccca7ed9efa1551699e7c99996fa1
prem023 <47606671+prem0023@users.noreply.github.com>
2019-02-14 20:06:41 +0530

    new way to print any string

--- first_code.py
+++ first_code.py
@@ -1,3 +1,13 @@
 print("Hello World")
 print("I am Phanishwar Nath")
 print("My nick name id Prem")
+
+
+def first(string):
+    print(string)
+
+
+a = input(str("Enter any string: "))
+
+
+first(a)

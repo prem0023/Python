7b8d54f963647f572f3e058aefc2a2822c5e1cea
prem023 <47606671+prem0023@users.noreply.github.com>
2019-02-14 21:12:41 +0530

    addition, subtraction, multiplication of two number

---
+++ add_sub_multiply.py
@@ -0,0 +1,15 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+print(add(a, b))
+print(subtract(a, b))
+print(multiply(a, b))

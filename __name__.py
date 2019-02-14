082ae510ae98bfd4ae2314fb4ad480ba36daf466
prem023 <47606671+prem0023@users.noreply.github.com>
2019-02-14 22:11:27 +0530

    use of __name__=="__main__"

---
+++ __name__.py
@@ -0,0 +1,6 @@
+print(__name__)
+if __name__:                 # here if we use  if __name__ =="__main__":  then this fn. will only execute in main program else we can import this fn.
+    print("Hello World")
+    print("How was the Day?")
+
+

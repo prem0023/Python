906e1b81ec2d88a51b969c33df7672b03b6866aa
prem023 <47606671+prem0023@users.noreply.github.com>
2019-02-14 21:09:33 +0530

    create array and search in it

---
+++ create_array_search_array.py
@@ -0,0 +1,19 @@
+from array import *
+
+arr = array('i',[])
+n = int(input("Enter the size of array: "))
+for i in range(n):
+    x = int(input("Enter the next number: "))
+    arr.append(x)
+print(arr)
+
+
+a = int(input("Enter the number to search"))
+k = 0
+for e in arr:
+    if e == a:
+        print(k)
+        break
+    k +=1
+
+print(arr.index(a))

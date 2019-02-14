ae2a700935ca0862e117b8e6f2f0ae9f2982dba4
prem023 <47606671+prem0023@users.noreply.github.com>
2019-02-14 20:16:01 +0530

    Create sell_candey.py

---
+++ sell_candey.py
@@ -0,0 +1,14 @@
+av = 5
+a = int(input("how many candey you want: "))
+i = 1
+
+while i<=a:
+
+    if i>av:
+        print("out of stock")
+        break
+
+    print("candey")
+    i +=1
+
+print("Thanks for shopping"

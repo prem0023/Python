2f294d27efa00b337e5c5bdee7bbd931e675591b
prem023 <47606671+prem0023@users.noreply.github.com>
2019-02-14 22:23:44 +0530

    multiple class and use of first function on other class

---
+++ multi_class.py
@@ -0,0 +1,18 @@
+class pycharm:
+
+    def food(self):
+        print("maggie")
+        print("Chaat")
+        print("Lunch")
+
+
+class breakfast():
+
+    def lunch(self,new):
+        new.food()
+        print("Whats new?")
+        print("South indian food")
+
+new = pycharm()
+a = breakfast()
+a.lunch(new)

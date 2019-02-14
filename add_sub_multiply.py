607b58e95c6f0b4458d7e4a0bba9ad569a73c9d7
prem023 <47606671+prem0023@users.noreply.github.com>
2019-02-14 21:15:33 +0530

    division(quotient & integer part)

--- add_sub_multiply.py
+++ add_sub_multiply.py
@@ -10,6 +10,15 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+def div1(a, b):
+    return a/b
+def div2(a, b):
+    return a//b
+
 print(add(a, b))
 print(subtract(a, b))
 print(multiply(a, b))
+
+print(div1(a, b))
+print(div2(a, b))
+

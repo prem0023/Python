46f7b13328a407e6a13584c32c330fd84b250ba0
prem023 <47606671+prem0023@users.noreply.github.com>
2019-02-15 20:55:27 +0530

    we can search an element from a list

---
+++ linear_search.py
@@ -0,0 +1,22 @@
+pos = 0
+
+def search(list, n):
+    i = 0
+
+    while i < len(list):
+        if n ==list[i]:
+            global pos                  #globals()['pos']=i
+            pos = n
+            return True
+        i = i + 1
+
+    return False
+
+list = [1,2,3,4,22,27,0,5,6,12,25,55,7,8,9,10]
+
+n = int(input("Enter an integer to search in list: "))
+
+if search(list,n):
+    print("Found ", pos )
+else:
+    print("Not Found")

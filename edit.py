e8fdbe0312fc51b5f36209fb0bd8630cc098a4d8
prem023 <47606671+prem0023@users.noreply.github.com>
2019-02-15 20:32:31 +0530

    with the help of one text copy and edit it into another text

---
+++ edit.py
@@ -0,0 +1,10 @@
+f = open('Mydata', 'r')         #to read data writtrn in MyData file
+
+
+f1 = open('New', 'w')           #to write data in 'New' file if not exist then it will created automatecly, but it is temporary not save
+f1.write("My name.., ")         #it will write the data which inside " " 
+f1.write("My name is prem")     #it will write the data which inside " "
+f1.write("\n")                  #it will write the data which inside " " (here it will change line)
+
+for data in f:                  # it mean to MyData file
+    f1.write(data)              #write all data which is in 'MyData' to f1(i.e New)
